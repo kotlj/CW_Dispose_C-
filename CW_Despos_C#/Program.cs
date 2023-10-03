6b056e31d304b13e8f87a4eeb23fc32ffe8a0595
kotlj <crotlj6@gmail.com>
2023-10-03 19:02:28 +0300

    main

---
+++ CW_Despos_C#/Program.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CW_Despos_C_
+{
+    class Play
+    {
+        private string _Name;
+        private string _Author;
+        private string _Genre;
+        private DateTime _Date;
+        public string Name
+        {
+            get
+            {
+                return _Name;
+            }
+            set
+            {
+                _Name = value;
+            }
+        }
+        public string Author
+        {
+            get
+            {
+                return _Author;
+            }
+            set
+            {
+                _Author = value;
+            }
+        }
+        public string Genre
+        {
+            get
+            {
+                return _Genre;
+            }
+            set
+            {
+                _Genre = value;
+            }
+        }
+        public DateTime Date
+        {
+            get
+            {
+                return _Date;
+            }
+            set
+            {
+                _Date = value;
+            }
+        }
+
+        public Play(string name, string author, string genre, DateTime date)
+        {
+            Name = name;
+            Author = author;
+            Genre = genre;
+            Date = date;
+        }
+        ~Play()
+        {
+            Console.WriteLine("Object was deleted");
+        }
+    }
+    class Shop : IDisposable
+    {
+        private string _Name;
+        private string _Adress;
+        private string _Type;
+        
+        public string Name
+        {
+            get
+            {
+                return _Name;
+            }
+            set
+            {
+                _Name = value;
+            }
+        }
+        public string Adress
+        {
+            get
+            {
+                return _Adress;
+            }
+            set
+            {
+                _Adress = value;
+            }
+        }
+        public string Type
+        {
+            get
+            {
+                return _Type;
+            }
+            set
+            {
+                _Type = value;
+            }
+        }
+
+        public Shop(string name, string adress, string type)
+        {
+            _Name = name;
+            _Adress = adress;
+            _Type = type;
+        }
+        public void Dispose()
+        {
+            Console.WriteLine("Object was disposed");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime date = new DateTime(1603);
+            Play play = new Play("Macbeth", "William Shakespeare", "War, drama, history", date);
+            GC.Collect();
+            Shop shop = new Shop("Shop", "Somewhere", "Electronic");
+            try
+            {
+                shop = new Shop("Shop", "Somewhere", "Electronic");
+            }
+            finally
+            {
+                shop.Dispose();
+            }
+        }
+    }
+}
---
+++ CW_Despos_C#/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CW_Despos_C#")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CW_Despos_C#")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("22acdda6-5c7e-4b7f-ba24-1e8262abc9f8")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
